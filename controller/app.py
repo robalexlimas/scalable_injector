ab358f1468223f36af258c7c619a1e1cdf584df9
Robert Limas <robalex123@outlook.es>
2023-02-15 16:16:22 +0100

    controller

--- controller/app.py
+++ controller/app.py
@@ -1,32 +1,50 @@
-from common import error, response
+from common import APPS, GPUS
+
+import random
 
 
-from flask import Flask
-from flask_cors import CORS
+def exhaustive_fault_list():
+    fault_list = []
+    for app in APPS:
+        for gpu in GPUS:
+            arch = GPUS[gpu]
+            for sm in range(arch['SM']):
+                for sub_core in range(arch['SM_SUB_CORE']):
+                    for core_type in arch['CORE_TYPE']:
+                        for core_id in range(arch['CORE_ID']):
+                            for in_out in arch['IN_OUT']:
+                                for operand in arch['OPERAND']:
+                                    for mask in range(32):
+                                        for bit in [0, 1]:
+                                            fault = '1,1,{},{},{},{},{},{},{},{},{},{}\n'.format(
+                                                app,
+                                                sm,
+                                                sub_core,
+                                                core_type,
+                                                core_id,
+                                                in_out,
+                                                operand,
+                                                2 ** mask,
+                                                bit,
+                                                gpu
+                                            )
+                                            fault_list.append(fault)
+    return fault_list
 
 
-app = Flask(__name__)
-CORS(app)
+def random_list(faults_list, n):
+    fault_list = random.sample(faults_list, n)
+    return fault_list
 
 
-@app.route('/', methods=['GET'])
-def info():
-    data = {
-        'organization': 'Politecnico di Torino',
-        'group': 'CAD',
-        'author': 'Robert Alexander Limas Sierra'
-    }
-    return response('Injector', data=data)
-
-
-@app.route('/fault', methods=['GET'])
-def fault():
-    data = {
-        'fault': '',
-        'app': 'nn'
-    }
-    return response('Fault information', data=data)
+def save_csv(fault_list, filename):
+    with open(filename, 'w') as file:
+        file.writelines(fault_list)
 
 
 if __name__ == '__main__':
-    app.run()
+    exhaustive = exhaustive_fault_list()
+    print(len(exhaustive))
+    random = random_list(exhaustive, 100)
+    save_csv(exhaustive, 'exhaustive.csv')
+    save_csv(random, 'random.csv')
--- controller/common.py
+++ controller/common.py
@@ -1,10 +1,48 @@
-from flask import make_response, jsonify
+import os
 
 
-def response(message, data=None, status=200):
-    res = {'message': message, 'data': data} if data else {'message': message}
-    return make_response(jsonify(res), status)
+BASE_DIR = os.getcwd()
 
+APPS = [
+    'backprop',
+    'gaussian',
+    'nn',
+    'lavamd',
+    'lud',
+    'heartwall'
+]
 
-def error(message, status=400):
-    return response(message, status=status)
+GPUS = {
+    'SM75_RTX2060': {
+        'SM': 30,
+        'SM_SUB_CORE': 4,
+        'CORE_TYPE': [2, 5, 6],
+        'CORE_ID': 16,
+        'IN_OUT': [0, 1],
+        'OPERAND': [0, 1, 2]
+    }, 
+    'SM7_TITANV': {
+        'SM': 80,
+        'SM_SUB_CORE': 4,
+        'CORE_TYPE': [2, 5, 6],
+        'CORE_ID': 16,
+        'IN_OUT': [0, 1],
+        'OPERAND': [0, 1, 2]
+    },
+    'SM6_TITANX': {
+        'SM': 28,
+        'SM_SUB_CORE': 4,
+        'CORE_TYPE': [2, 5, 6],
+        'CORE_ID': 16,
+        'IN_OUT': [0, 1],
+        'OPERAND': [0, 1, 2]
+    },
+    'SM3_KEPLER_TITAN': {
+        'SM': 14,
+        'SM_SUB_CORE': 4,
+        'CORE_TYPE': [2, 5, 6],
+        'CORE_ID': 16,
+        'IN_OUT': [0, 1],
+        'OPERAND': [0, 1, 2]
+    }
+}
